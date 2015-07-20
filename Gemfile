2e146a2c343a881efc9648873dcdd28028b2295f
Kristin Houchin <kmhouchin@gmail.com>
2015-07-19 23:40:34 -0500

    setting up the bundler stuff before bed

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "pry"
+gem "activerecord"
+gem "sinatra"
+gem "sinatra-contrib"
+gem "activesupport"
+
+gem 'sqlite3', :groups => [:development, :test]
+gem 'pg', :groups => [:production]
--- app.rb
+++ app.rb
@@ -1,10 +1,31 @@
+require 'rubygems'
+require 'bundler/setup'
+
 require "pry"
 require "active_record"
-require "sqlite3"
 require "sinatra"
 require "sinatra/reloader"
 
+
+configure :development do
+  require "sqlite3"
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'collection.db')
+end
+
+configure :production do
+  require "pg"
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
 
 ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
 
